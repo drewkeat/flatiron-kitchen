2d1cad419c0b3dc61b5b1e44b7cc8b4cd00cfe95
Drew Keat <thedrewkeat@gmail.com>
2021-08-19 15:57:59 -0600

    stub recipe views and controller actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,34 @@
 class RecipesController < ApplicationController
+
+    def index
+        @recipes = Recipe.all
+    end
+
+    def new
+        @recipe = Recipe.new
+    end
+    
+    def create
+        @recipe = Recipe.new(recipe_params)
+        if @recipe.save
+          flash[:success] = "Recipe successfully created"
+          redirect_to @recipe
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+    
+    def show
+        @recipe = Recipe.find(params[:id])
+    end
+    
+    private
+    def recipe_params
+        params.require(:recipe).permit(:name)
+    end
 end
