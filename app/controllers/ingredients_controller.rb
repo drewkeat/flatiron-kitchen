bb48611018b000444302c6c1f385f8771da660ac
Drew Keat <thedrewkeat@gmail.com>
2021-08-19 15:48:53 -0600

    create ingredient views and controller actions

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,42 @@
 class IngredientsController < ApplicationController
+    def new
+        @ingredient = Ingredient.new
+    end
+
+    def create
+        @ingredient = Ingredient.new(ingredient_params)
+        if @ingredient.save
+          flash[:success] = "Ingredient successfully created"
+          redirect_to @ingredient
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    def show
+        @ingredient = Ingredient.find(params[:id])
+    end
+
+    def edit
+        @ingredient = Ingredient.find(params[:id])
+    end
+
+    def update
+        @ingredient = Ingredient.find(params[:id])
+        if @ingredient.update_attributes(ingredient_params)
+          flash[:success] = "Ingredient was successfully updated"
+          redirect_to @ingredient
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+    
+    
+    private
+    def ingredient_params
+        params.require(:ingredient).permit(:name)
+    end
+    
 end
