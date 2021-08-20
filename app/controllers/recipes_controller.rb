8d067b271be9bfdd30ad78840cf6e63835f9cb5b
Drew Keat <thedrewkeat@gmail.com>
2021-08-19 21:59:04 -0600

    done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,12 +6,13 @@ class RecipesController < ApplicationController
 
     def new
         @recipe = Recipe.new
+        @ingredients = Ingredient.all
     end
     
     def create
         @recipe = Recipe.new(recipe_params)
         if @recipe.save
-          flash[:success] = "Recipe successfully created"
+        flash[:success] = "Recipe successfully created"
           redirect_to @recipe
         else
           flash[:error] = "Something went wrong"
@@ -21,7 +22,20 @@ class RecipesController < ApplicationController
 
     def edit
         @recipe = Recipe.find(params[:id])
+        @ingredients = Ingredient.all
     end
+
+    def update
+        @recipe = Recipe.find(params[:id])
+        if @recipe.update_attributes(recipe_params)
+          flash[:success] = "Recipe was successfully updated"
+          redirect_to @recipe
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+    
     
     def show
         @recipe = Recipe.find(params[:id])
@@ -29,6 +43,6 @@ class RecipesController < ApplicationController
     
     private
     def recipe_params
-        params.require(:recipe).permit(:name)
+        params.require(:recipe).permit(:name, ingredient_ids:[])
     end
 end
